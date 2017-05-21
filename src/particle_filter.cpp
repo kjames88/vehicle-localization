58f6cfe24f82159af047a8bd2eda0d21a5b6f7d6
Kevin James <kjames4@gmail.com>
2017-05-21 16:41:13 -0700

    Error in bounds on some runs

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -29,7 +29,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
   normal_distribution<double> dist_x(x, std[0]);
   normal_distribution<double> dist_y(y, std[1]);
   normal_distribution<double> dist_theta(theta, std[2]);
-  num_particles = 1000;
+  num_particles = 100;
   for (int i=0; i<num_particles; i++) {
     Particle p;
     p.id = i;
@@ -37,8 +37,9 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
     p.y = dist_y(gen);
     p.theta = dist_theta(gen);
     p.weight = 1.0;
-    
+
     particles.push_back(p);
+    weights.push_back(1.0);
   }
   
   is_initialized = true;
@@ -67,18 +68,20 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
   default_random_engine gen;
-  normal_distribution<double> dist_v(velocity, std_pos[0]);    // FIXME
-  normal_distribution<double> dist_yaw(yaw_rate, std_pos[1]);  // FIXME
   double vtd = velocity / yaw_rate;
   double tdt = delta_t * yaw_rate;
+  // use 0-mean gaussians and add to state instead of creating new distributions for each particle
+  normal_distribution<double> dist_x(0, std_pos[0]);
+  normal_distribution<double> dist_y(0, std_pos[1]);
+  normal_distribution<double> dist_theta(0, std_pos[2]);
   for (int i=0; i<num_particles; i++) {
     double theta_0 = particles.at(i).theta;
     double x_f = particles.at(i).x + (vtd * (sin(theta_0 + tdt) - sin(theta_0)));
     double y_f = particles.at(i).y + (vtd * (cos(theta_0) - cos(theta_0 + tdt)));
     double theta_f = theta_0 + tdt;
-    particles.at(i).x = x_f;
-    particles.at(i).y = y_f;
-    particles.at(i).theta = theta_f;
+    particles.at(i).x = x_f + dist_x(gen);
+    particles.at(i).y = y_f + dist_y(gen);
+    particles.at(i).theta = theta_f + dist_theta(gen);
   }
 }
 
@@ -103,8 +106,13 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+  // std::cout << "Map:" << std::endl;
+  // for (auto it=map_landmarks.landmark_list.begin(); it!=map_landmarks.landmark_list.end(); it++) {
+  //   cout << "id=" << it->id_i << " x=" << it->x_f << " y=" << it->y_f << std::endl;
+  // }
   
-  double sigma[2][2] = {std_landmark[0], 0, 0, std_landmark[1]};
+  double sigma[2][2] = {pow(std_landmark[0],2), 0, 0, pow(std_landmark[1],2)};
   int m = observations.size();
   
   for (int p=0; p<num_particles; p++) {
@@ -129,8 +137,8 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
       obs.id = 0;
       obs_world.push_back(obs);
 
-      std::cout << "particle x=" << xt << " y=" << yt << " theta=" << particle.theta <<
-        " obs x=" << xobs << " y=" << yobs << " world obs x=" << xw << " y=" << yw << std::endl;
+      // std::cout << "particle x=" << xt << " y=" << yt << " theta=" << particle.theta <<
+      //   " obs x=" << xobs << " y=" << yobs << " world obs x=" << xw << " y=" << yw << std::endl;
     }
     
     // Establish correspondences
@@ -140,24 +148,24 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
     candidates.resize(m);
     int c = 0;
     for (auto ito = obs_world.begin(); ito != obs_world.end(); ito++) {
-      // Exclude any observations that are out of sensor range
-      double dist = sqrt(pow(ito->x - xt, 2) + pow(ito->y - yt, 2));      
-      if (dist <= sensor_range) {
-        for (auto itm = map_landmarks.landmark_list.begin(); itm != map_landmarks.landmark_list.end(); itm++) {
-          // Exclude any landmarks that are out of sensor range
-          double dist = sqrt(pow(itm->x_f - xt, 2) + pow(itm->y_f - yt, 2));
-          int landmark_id = itm->id_i;
-          if (dist <= sensor_range) {
+      for (auto itm = map_landmarks.landmark_list.begin(); itm != map_landmarks.landmark_list.end(); itm++) {
+        // Exclude any landmarks that are out of sensor range
+        double dist = sqrt(pow(itm->x_f - xt, 2) + pow(itm->y_f - yt, 2));
+        int landmark_id = itm->id_i;
+        if (dist <= (sensor_range + 10.0)) {
+          double dist = sqrt(pow(itm->x_f - ito->x, 2) + pow(itm->y_f - ito->y, 2));
+          // Exclude any association with distance above 10m
+          if (dist < 10.0) {
             // Compute the landmark and observation angles and exclude any landmark if angle differs by +/- pi/8 radians
-            double rho = atan2(ito->y, ito->x);
-            double landmark_rho = atan2(itm->y_f, itm->x_f);
-            if (abs(rho - landmark_rho) < M_PI/8) {
-              candidates.at(c).push_back(landmark_id);  // list of landmark IDs that could associate with this observation
-              std::cout << "accept landmark " << landmark_id << " world obs x=" << ito->x << " y=" << ito->y << std::endl;
-            }
+            //double rho = atan2(ito->y, ito->x);
+            //double landmark_rho = atan2(itm->y_f, itm->x_f);
+            //if (abs(rho - landmark_rho) < M_PI/8) {
+            candidates.at(c).push_back(landmark_id);  // list of landmark IDs that could associate with this observation
+            //std::cout << "candidate " << c << " accept landmark " << landmark_id << " x=" << itm->x_f << " y=" << itm->y_f << " world obs x=" << ito->x << " y=" << ito->y << std::endl;
+            //}
           }
         }
-      }
+      }      
       c++;
     }
     // Compute multivariate gaussian for each combination of reasonable associations
@@ -166,22 +174,32 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 
     bool fail = false;
     for (int i=0; i<m; i++) {
-      if (candidates.at(i).empty())
+      if (candidates.at(i).empty()) {
+        std::cout << "world obs x=" << obs_world.at(i).x << " y=" << obs_world.at(i).y << " has no landmark option" << std::endl;
         fail = true;
+      }
     }
     if (fail == false) {
       double w_max = 0.0;
+      int idx_max = 0;
       std::vector<std::vector<int> > sets = recursive_assoc(candidates, 0);
-      for (auto it=sets.begin(); it!=sets.end(); it++) {
-        double w = mv_gaussian(obs_world, *it, sigma, map_landmarks);
+      for (int idx=0; idx<sets.size(); idx++) {
+        double w = mv_gaussian(obs_world, sets.at(idx), sigma, map_landmarks);
         if (w > w_max) {
           w_max = w;
-          particles.at(p).associations = *it;
+          idx_max = idx;
         }
       }
+      std::vector<double> sense_x;
+      std::vector<double> sense_y;
+      for (int i=0; i<obs_world.size(); i++) {
+        sense_x.push_back(obs_world.at(i).x);
+        sense_y.push_back(obs_world.at(i).y);
+      }
+      particles.at(p) = SetAssociations(particles.at(p), sets.at(idx_max), sense_x, sense_y);
       weights[p] = w_max;
     } else {
-      weights[p] = 0.0001;
+      weights[p] = 0.0;
     }
     particles.at(p).weight = weights[p];
   }
@@ -228,19 +246,26 @@ double ParticleFilter::mv_gaussian(std::vector<LandmarkObs> const& observations,
 
   double mult = 1.0 / (sigma[0][0] * sigma[1][1] - sigma[0][1] * sigma[1][0]);
   double sigma_inv[2][2] = {mult * sigma[1][1], -1.0 * mult * sigma[0][1], -1.0 * mult * sigma[1][0], mult * sigma[0][0]};
+  double det_sigma = ((2.0 * M_PI * sigma[0][0]) * (2.0 * M_PI * sigma[1][1])) - ((2.0 * M_PI * sigma[0][1]) * (2.0 * M_PI * sigma[1][0]));
   
   assert(observations.size() == associations.size());
-  double w = 0.0;
+  double w = 1.0;
   int m = observations.size();
   for (int i=0; i<m; i++) {
     double delta[2];
-    Map::single_landmark_s landmark = landmark_map.landmark_list.at(associations.at(i));
+    // landmark id is 1-based: adjust it back to 0-based vector position
+    Map::single_landmark_s landmark = landmark_map.landmark_list.at(associations.at(i) - 1);
     delta[0] = observations.at(i).x - landmark.x_f;
     delta[1] = observations.at(i).y - landmark.y_f;
+
+    //std::cout << "landmark " << landmark.id_i << " delta x=" << delta[0] << " y=" << delta[1] << std::endl;
+    
     double e1[2] = {sigma_inv[0][0] * delta[0] + sigma_inv[0][1] * delta[1],
                     sigma_inv[1][0] * delta[0] + sigma_inv[1][1] * delta[1]};
     double e = -0.5 * (delta[0] * e1[0] + delta[1] * e1[1]);
     e = exp(e);
+    e /= sqrt(det_sigma);
+    w *= e;
   }
   
   return w;
@@ -251,6 +276,28 @@ void ParticleFilter::resample() {
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::discrete_distribution<> dist(weights.begin(), weights.end());
+  std::vector<Particle> new_particles;
+  for (int i=0; i<num_particles; i++) {
+    int p = dist(gen);
+    new_particles.push_back(particles.at(p));
+  }
+
+  // std::cout << "particles before: ";
+  // for (int i=0; i<num_particles; i++) {
+  //   std::cout << particles.at(i).x << "," << particles.at(i).y << "," << particles.at(i).weight << " ";
+  // }
+  // std::cout << endl;
+  
+  particles = new_particles;
+
+  // std::cout << "particles after: ";
+  // for (int i=0; i<num_particles; i++) {
+  //   std::cout << particles.at(i).x << "," << particles.at(i).y << "," << particles.at(i).weight << " ";
+  // }
+  // std::cout << endl;
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)
